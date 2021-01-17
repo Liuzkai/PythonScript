e4c9b1d1feaed793ff4b536ca398b9c38882f78c
LiuzKai <liuzkai2012@gmail.com>
2021-01-17 16:32:25 +0800

    change name

---
+++ FluentPython/Clockdeco_demo.py
@@ -0,0 +1,12 @@
+import time
+
+def clock(func):
+    def clocked(*args):
+        t0 = time.perf_counter()
+        result = func(*args)
+        elapsed = time.perf_counter() - t0
+        name = func.__name__
+        arg_str = ",".join([repr(arg) for arg in args])
+        print("[%0.8fs], %s(%s)->%s"%(elapsed, name, arg_str, result))
+        return result
+    return clocked
